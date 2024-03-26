e7af837e9315ed2e250562e9bae9e9eff0893ba8
Daniel <123848323+Daniellaurin@users.noreply.github.com>
2024-03-25 20:53:55 -0400

    TUN Protocol
    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+accounter an issue with external libraries 
+
+had to #define TUNSETIFF _IOW('T', 202, int)
+
+
+Ressources for TUN/TAP: 
+
+https://www.kernel.org/doc/Documentation/networking/tuntap.txt
+https://github.com/saminiir/level-ip/blob/master/src/tuntap_if.c
+http://www.saminiir.com/lets-code-tcp-ip-stack-1-ethernet-arp/
---
+++ tun.c
@@ -0,0 +1,42 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+// #include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#define TUNSETIFF _IOW('T', 202, int)
+
+int tun_alloc(char *dev) {
+  struct ifreq ifr;
+  int fd, err;
+
+  // Attempt to open the TUN device file with read-write permissions
+  if ((fd = open("/dev/net/tap", O_RDWR)) < 0)
+    print_error("Cannot open TUN/TAP dev");
+  exit(1);
+
+  // Clear the ifreq structure
+  clear(ifr);
+
+  /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
+   *        IFF_TAP   - TAP device
+   *
+   *        IFF_NO_PI - Do not provide packet information
+   */
+
+  // Set the flags for the TUN device (no Ethernet headers)
+  ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+  if (*dev) {
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+  }
+  // Use ioctl to configure the TUN device with the flags and name
+  if ((err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0) {
+    print_error("ERR: Could not ioctl tun: %s\n", strerror(errno));
+    close(fd);
+    return err;
+  }
+  // Copy the device name back to the caller
+  strcpy(dev, ifr.ifr_name);
+  return fd;
+}
